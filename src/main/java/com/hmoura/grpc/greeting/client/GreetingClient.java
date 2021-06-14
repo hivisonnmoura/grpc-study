6b4f619825b995c346192541f6d2c8bc17a33dd4
hivison.moura <hivson.moura@sinch.com>
2021-06-14 10:01:03 -0300

    Adding a unary API (greeting service)

--- src/main/java/com/hmoura/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/hmoura/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,9 @@
 package com.hmoura.grpc.greeting.client;
 
-import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -12,11 +15,26 @@ public class GreetingClient {
                 .usePlaintext().build();
 
         System.out.println("Creating stub");
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
 
+        //old and dummy
+        //DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
         //DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.DummyServiceFutureStub.newStub(channel);
 
-        // do something
+        //Created a greet service client (blocking -> synchronous)
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        //Created a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder().setFirstName("Hivison").setLastName("Moura").build();
+
+        //Do the same for a GreetRequest
+        GreetRequest greetRequest = GreetRequest.newBuilder().setGreeting(greeting).build();
+
+        //Call the RPC and get back a GreetResponse (protocol buffers)
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+
+        System.out.println(greetResponse.getResult());
+
+        //Do something
         System.out.println("Shutting down channel");
         channel.shutdown();
     }
--- src/main/java/com/hmoura/grpc/greeting/server/GreetingServer.java
+++ src/main/java/com/hmoura/grpc/greeting/server/GreetingServer.java
@@ -1,20 +1,22 @@
-package com.hmoura.grpc.greeting.service;
+package com.hmoura.grpc.greeting.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 import java.io.IOException;
 
-public class GreetingService {
+public class GreetingServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Hello gRPC");
 
-        Server server = ServerBuilder.forPort(50051).build();
+        Server server = ServerBuilder.forPort(50051)
+                .addService(new GreetServiceImpl())
+                .build();
 
         server.start();
 
-        Runtime.getRuntime().addShutdownHook(new Thread( () -> {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Received Shutdown Request");
             server.shutdown();
             System.out.println("Successfully stopped the server");
