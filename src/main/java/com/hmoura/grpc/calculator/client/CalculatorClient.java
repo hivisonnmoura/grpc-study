bb187de63d484f565b223b3af323e859666fe1f1
hivison.moura <hivson.moura@sinch.com>
2021-06-14 14:48:10 -0300

    Adding a Server stream demo

--- src/main/java/com/hmoura/grpc/calculator/client/CalculatorClient.java
+++ src/main/java/com/hmoura/grpc/calculator/client/CalculatorClient.java
@@ -1,6 +1,7 @@
 package com.hmoura.grpc.calculator.client;
 
 import com.proto.calculator.CalculatorServiceGrpc;
+import com.proto.calculator.PrimeNumberDecompositionRequest;
 import com.proto.calculator.SumRequest;
 import com.proto.calculator.SumResponse;
 import io.grpc.ManagedChannel;
@@ -18,6 +19,8 @@ public class CalculatorClient {
         //Created a calculator service client (blocking -> synchronous)
         CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorClient = CalculatorServiceGrpc.newBlockingStub(channel);
 
+        //Unary
+        /*
         //Do the same for a CalculatorRequest
         SumRequest request = SumRequest.newBuilder().setFirstValue(3).setSecondValue(10).build();
 
@@ -25,7 +28,15 @@ public class CalculatorClient {
         SumResponse response = calculatorClient.sum(request);
         //print the result
         System.out.println(request.getFirstValue() + " + " + request.getSecondValue() + " = " + response.getResult());
+*/
 
+        //Server Streaming
+        PrimeNumberDecompositionRequest request =
+                PrimeNumberDecompositionRequest.newBuilder()
+                .setNumber(123123123).build();
+        calculatorClient.primeNumberDecomposition(request).forEachRemaining(
+                primeResponse -> System.out.println(primeResponse.getPrimeFactor())
+        );
         //Shutdown the channel
         System.out.println("Shutting down channel");
         channel.shutdown();
--- src/main/java/com/hmoura/grpc/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/com/hmoura/grpc/calculator/service/CalculatorServiceImpl.java
@@ -1,9 +1,13 @@
 package com.hmoura.grpc.calculator.service;
 
 import com.proto.calculator.CalculatorServiceGrpc;
+import com.proto.calculator.PrimeNumberDecompositionRequest;
+import com.proto.calculator.PrimeNumberDecompositionResponse;
 import com.proto.calculator.SumRequest;
 import com.proto.calculator.SumResponse;
 import io.grpc.stub.StreamObserver;
+
+import java.util.stream.IntStream;
 
 public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
 
@@ -23,4 +27,21 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
         //Complete the RPC call
         responseObserver.onCompleted();
     }
+
+    @Override
+    public void primeNumberDecomposition(PrimeNumberDecompositionRequest request, StreamObserver<PrimeNumberDecompositionResponse> responseObserver) {
+        factors(request.getNumber()).forEach(factor ->
+                responseObserver.onNext(PrimeNumberDecompositionResponse.newBuilder().setPrimeFactor(factor).build()));
+
+        responseObserver.onCompleted();
+    }
+
+    private IntStream factors(int num) {
+        return IntStream.range(2, num)
+                .filter(x -> num % x == 0)
+                .mapToObj(x -> IntStream.concat(IntStream.of(x), factors(num / x)))
+                .findFirst()
+                .orElse(IntStream.of(num));
+    }
+
 }
